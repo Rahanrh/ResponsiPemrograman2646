284cc7ec710ea4579d29011839ae6dcae0d9b58f
Rahanrh <61685263+Rahanrh@users.noreply.github.com>
2020-07-06 18:11:23 +0700

    Responsi Pemrograman 2646

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        public string kodebarang { get; set; }
+        public string namabarang { get; set; }
+        public string hargabeli { get; set; }
+        public string hargajual { get; set; }
+
+    }
+}
